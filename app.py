e1ca07ffbdf0858b307f76e32b8745a97a86b57e
il88li <yl9667383@gmail.com>
2026-07-11 19:33:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import threading
 from models import db, User, Pattern, Category, PromptLibrary, LibraryAd, Notification, Ad, SiteSetting, PageVisit, AdView
 from config import Config
 
-app = Flask(__name__, template_folder=os.path.dirname(os.path.abspath(__file__)))
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 app.config.from_object(Config)
 app.config.update(
     SESSION_SQLALCHEMY=db,
